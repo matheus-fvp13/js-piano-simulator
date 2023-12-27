68413ec0ae66d4ffd2c47b7e78a7ce6930bf2830
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-12-27 16:23:57 -0300

    feat - volume control

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,7 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys__key");
+const volumeSlider = document.querySelector(".volume-slider input");
 const mapedKey = [];
-
-let audio = new Audio("src/tunes/a.wav");
+const audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`
@@ -14,6 +14,10 @@ const playTune = (key) => {
     }, 150)
 }
 
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+
 pianoKeys.forEach((key) => {
     key.addEventListener("click", () => playTune(key.dataset.key))
     mapedKey.push(key.dataset.key);
@@ -23,3 +27,5 @@ pianoKeys.forEach((key) => {
 document.addEventListener("keydown", (e) => {
     if(mapedKey.includes(e.key)) playTune(e.key);
 });
+
+volumeSlider.addEventListener("input", handleVolume);
